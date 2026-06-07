6c12140b594d46ef5847432fe01ca208ac44586b
Nandini-tech-path <silverunandini@gmail.com>
2026-06-07 09:03:03 +0530

    Switch to OutOfProcess hosting model

--- VotingSystem.Web/obj/Release/net8.0/VotingSystem.Web.AssemblyInfo.cs
+++ VotingSystem.Web/obj/Release/net8.0/VotingSystem.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a46e21b3a3cf0e261179ed86047c45e16dfd9eda")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
